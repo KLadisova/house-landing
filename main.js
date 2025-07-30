fcf589e77a643c10f37d20248562c5328c92e643
Kladisova <karina710@inbox.lv>
2025-07-30 15:39:24 +0300

    first btn

---
+++ main.js
@@ -0,0 +1,13 @@
+// const button = document.getElementById("btn-rectangle");
+// button.addEventListener("click", function () {
+//   button.style.backgroundColor = "#376B44";
+//   button.style.color = "#FFFFFF";
+// });
+const buttons = document.querySelectorAll(".btn-rectangle");
+
+buttons.forEach(button => {
+    button.addEventListener("click", function () {
+      button.style.backgroundColor = "#376B44"; 
+      button.style.color = "#FFFFFF";           
+    });
+  });
